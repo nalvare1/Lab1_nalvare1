1fbaafebeef113bcdd02244dfe9caa52c7615a18
Natalie Alvarez <nalvare1@nd.edu>
2017-02-02 11:35:48 -0500

    Initial commit

---
+++ app/src/main/java/com/cs60333/nalvare1/lab1_nalvare1/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.cs60333.nalvare1.lab1_nalvare1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+        int i = 0;
+    }
+}
