667a271abe20839ec674cb009477740879cf823c
Natalie Alvarez <nalvare1@nd.edu>
2017-02-09 12:09:16 -0500

    "commit: Lab 3b"

--- app/src/main/java/com/cs60333/nalvare1/lab1_nalvare1/MainActivity.java
+++ app/src/main/java/com/cs60333/nalvare1/lab1_nalvare1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cs60333.nalvare1.lab1_nalvare1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,6 +8,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,18 +22,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //String[] teams = {"Ohio State", "Florida State", "Wake Forest", "Boston College", "North Carolina State", "Georgia Tech", "North Virginia", "Chicago State"};
+        String[] wake = {"wf", "Wake Forest", "Feb 2"};
+        String[] ohio = {"os", "Ohio State", "Feb 4"};
+        String[] florida = {"fsu", "Florida State", "Feb 11"};
+        String[] boston = {"bc", "Boston College", "Feb 14"};
+        String[] northC = {"ncs", "North Carolina State", "Feb 18"};
+        String[] georgia = {"gt", "Georgia Tech", "Feb 26"};
+        String[] northV = {"nv", "North Virginia", "March 1"};
+        String[] chicago = {"cs", "Chicago State", "March 4"};
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        */
+        //create a list of arrays
+        ArrayList<String[]> arr = new ArrayList<String[]>();
+        arr.add(wake);
+        arr.add(ohio);
+        arr.add(florida);
+        arr.add(boston);
+        arr.add(northC);
+        arr.add(georgia);
+        arr.add(northV);
+        arr.add(chicago);
+
+
+        ScheduleAdapter scheduleAdapter = new ScheduleAdapter(getApplicationContext(), arr);
+
+        ListView scheduleListView = (ListView) findViewById(R.id.scheduleListView);
+        scheduleListView.setAdapter(scheduleAdapter);
+
+
+
     }
-
 }
---
+++ app/src/main/java/com/cs60333/nalvare1/lab1_nalvare1/ScheduleAdapter.java
@@ -0,0 +1,48 @@
+package com.cs60333.nalvare1.lab1_nalvare1;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+/**
+ * Created by apple on 2/8/17.
+ */
+
+public class ScheduleAdapter extends ArrayAdapter<String[]> {
+    ScheduleAdapter (Context context, ArrayList<String[]> schedule) {
+        super(context, R.layout.schedule_item, schedule);
+    }
+
+    public String getPackageName() {
+        return "com.cs60333.nalvare1.lab1_nalvare1";
+    }
+
+    public View getView (int position, View convertView, ViewGroup parent) {
+        LayoutInflater scheduleInflater = LayoutInflater.from(getContext());
+        View scheduleView = scheduleInflater.inflate(R.layout.schedule_item, parent, false);
+
+        String matchItem[] = getItem(position);
+
+        ImageView teamLogo = (ImageView) scheduleView.findViewById(R.id.teamLogo);
+        String mDrawableName = matchItem[0];
+        int resID = getContext().getResources().getIdentifier(mDrawableName, "drawable", getContext().getPackageName());
+        teamLogo.setImageResource(resID);
+
+        TextView teamName = (TextView) scheduleView.findViewById(R.id.scheduleText);
+        teamName.setText(matchItem[1]);
+
+        TextView gameDate = (TextView) scheduleView.findViewById(R.id.gameText);
+        gameDate.setText(matchItem[2]);
+
+        return scheduleView;
+    }
+
+}
