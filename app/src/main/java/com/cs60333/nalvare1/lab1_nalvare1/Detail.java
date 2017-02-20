9baaedc992d618fadc955e055dc46d9d94474ddd
Natalie Alvarez <nalvare1@nd.edu>
2017-02-19 22:59:03 -0500

    "commit: Lab 4 (final)"

--- app/src/main/java/com/cs60333/nalvare1/lab1_nalvare1/Detail.java
+++ app/src/main/java/com/cs60333/nalvare1/lab1_nalvare1/Detail.java
@@ -1,9 +1,7 @@
 package com.cs60333.nalvare1.lab1_nalvare1;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,6 +20,7 @@ public class Detail extends Activity {
 
         Button cameraButton = (Button) findViewById(R.id.cameraButton);
         ImageView opposingTeamImage = (ImageView) findViewById(R.id.opposingTeamImage);
+        ImageView notreDameImage = (ImageView) findViewById(R.id.notreDameImage);
         //**set image!!opposingTeamImage.set
 
         TextView dateText = (TextView) findViewById(R.id.dateText);
@@ -31,26 +30,29 @@ public class Detail extends Activity {
         TextView opposingTeamText3 = (TextView) findViewById(R.id.opposingTeamText3);
 
 
-    //    View.OnClickListener cameraButtonClicked = (v) -> {
-    //        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-    //        startActivity(cameraIntent);
-     //   };
-     //   cameraButton.setOnClickListener(cameraButtonClicked);
+  //      View.OnClickListener cameraButtonClicked = (v) -> {
+  //          Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+  //          startActivity(cameraIntent);
+  //      };
+ //       cameraButton.setOnClickListener(cameraButtonClicked);
 
         String[] stringInfo = getIntent().getStringArrayExtra("team");
 
 
-//        dateText.setText(stringInfo[3]);
+        dateText.setText(stringInfo[3]);
 
-  //      opposingTeamText1.setText(stringInfo[1]);
-    //    opposingTeamText2.setText(stringInfo[4]);
-      //  opposingTeamText3.setText(stringInfo[5]);
+        opposingTeamText1.setText(stringInfo[1]);
+        opposingTeamText2.setText(stringInfo[4]);
+        opposingTeamText3.setText(stringInfo[5]);
 
-      //  scoreText2.setText(stringInfo[6]);
+        scoreText2.setText(stringInfo[6]);
 
-       // String mDrawableName = stringInfo[0];
-       // int resID = getContext().getResources().getIdentifier(mDrawableName, "drawable", getContext().getPackageName());
-       // teamLogo.setImageResource(resID);
+        String mDrawableName = stringInfo[0];
+        int resID2 = getApplicationContext().getResources().getIdentifier(mDrawableName, "drawable", getApplicationContext().getPackageName());
+        opposingTeamImage.setImageResource(resID2);
+
+        int resID3 = getApplicationContext().getResources().getIdentifier("fightingirishicon", "drawable", getApplicationContext().getPackageName());
+        notreDameImage.setImageResource(resID3);
 
     }
 }
